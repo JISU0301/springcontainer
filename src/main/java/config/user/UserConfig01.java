a68f772c6e82f9d129e47bd1ac4b495c2085c440
BIT <BIT@DESKTOP-NMF9F4U>
2019-10-14 10:54:17 +0900

    java config 심플 예제

---
+++ src/main/java/config/user/UserConfig01.java
@@ -0,0 +1,18 @@
+package config.user;
+
+import org.springframework.context.annotation.Bean;
+
+import kr.co.itcen.springcontainer.user.User;
+
+public class UserConfig01 {
+	
+	@Bean
+	public User user() {
+		User user = new User();
+		user.setNo(5L);
+		user.setName("도우넛");
+		
+		return user;
+	}
+
+}
